40ec8d1d446e65dcb077ef162f7e4c4f2fc38835
PREMIEREHELL <88470674+PREMIEREHELL@users.noreply.github.com>
2021-09-10 18:18:57 -0700

    Updated UI

--- SetPlayer_Position_Reenable_Achevements.py
+++ SetPlayer_Position_Reenable_Achevements.py
@@ -37,23 +37,25 @@ class SetPlayer(wx.Panel, DefaultOperationUI):
         self.YL.SetLabel("Y:")
         self.ZL = wx.StaticText(self, wx.LEFT)
         self.ZL.SetLabel("Z:")
-        self.dimL = wx.StaticText(self, wx.LEFT)
-        self.dimL.SetLabel(" 0 = Overworld\n 1 = Nether\n 2 = End")
         self.X = wx.TextCtrl(
-            self, style=wx.TE_LEFT | wx.TE_BESTWRAP, size=(90, 20),
+            self, style=wx.TE_LEFT | wx.TE_BESTWRAP, size=(150, 20),
         )
         self.Y = wx.TextCtrl(
-            self, style=wx.TE_LEFT | wx.TE_BESTWRAP, size=(90, 20),
+            self, style=wx.TE_LEFT | wx.TE_BESTWRAP, size=(150, 20),
         )
         self.Z= wx.TextCtrl(
-            self, style=wx.TE_LEFT | wx.TE_BESTWRAP, size=(90, 20),
+            self, style=wx.TE_LEFT | wx.TE_BESTWRAP, size=(150, 20),
         )
-        self.dim = wx.TextCtrl(
-            self, style=wx.TE_LEFT | wx.TE_BESTWRAP, size=(90, 20),
-        )
+        self.dimDict = {
+            "Overworld": 0,
+            "Nether": 1,
+            "End":2
+        }
+        self.dim = wx.RadioBox(self, size=(200,50),label='Select Dimension', choices=list(self.dimDict.keys()))
 
 
-        self.apply = wx.Button(self, size=(40, 20),label="Apply")
+
+        self.apply = wx.Button(self, size=(50, 30),label="Apply")
         self.apply.Bind(wx.EVT_BUTTON, self.savePosData)
         self.achieve = wx.Button(self, size=(160, 20), label="Re-enable achievements")
         self.achieve.Bind(wx.EVT_BUTTON, self.loadData)
@@ -69,24 +71,29 @@ class SetPlayer(wx.Panel, DefaultOperationUI):
             player.append(x)
 
         self.playerlist = wx.ListBox(self, size=(160, 95), choices=player)
+        self.playerlist.SetSelection(0)
+        self.playerlistrun(None)
         self.playerlist.Bind(wx.EVT_LISTBOX, self.playerlistrun)
 
         self._sizer.Add(self.infoRe, 0, wx.LEFT, 10)
         self._sizer.Add(self.achieve, 0, wx.LEFT, 20)
         self._sizer.Add(self.info, 0, wx.LEFT, 10)
         self._sizer.Add(self.playerlist, 0, wx.LEFT, 10)
+        self.Grid = wx.GridSizer(3,2,0,0)
+        self.Grid.Add(self.XL, 0, wx.LEFT, 5)
+        self.Grid.Add(self.X,0,wx.LEFT,-60)
+        self.Grid.Add(self.YL, 0, wx.LEFT, 5)
+        self.Grid.Add(self.Y, 0, wx.LEFT, -60)
+        self.Grid.Add(self.ZL, 0, wx.LEFT, 5)
+        self.Grid.Add(self.Z, 0, wx.LEFT, -60)
 
-        self._sizer.Add(self.XL, 0, wx.LEFT, 20)
-        self._sizer.Add(self.X,0,wx.LEFT,20)
-        self._sizer.Add(self.YL, 0, wx.LEFT, 20)
-        self._sizer.Add(self.Y, 0, wx.LEFT, 20)
-        self._sizer.Add(self.ZL, 0, wx.LEFT, 20)
-        self._sizer.Add(self.Z, 0, wx.LEFT, 20)
-        self._sizer.Add(self.dimL, 0, wx.LEFT, 20)
-        self._sizer.Add(self.dim, 0, wx.LEFT, 20)
-        self._sizer.Add(self.infoRai, 0, wx.LEFT, 20)
-        self._sizer.Add(self.gm_mode, 0, wx.LEFT, 20)
-        self._sizer.Add(self.apply, 0, wx.LEFT, 10)
+        self._sizer.Add(self.Grid)
+        self.Grid.Fit(self)
+
+        self._sizer.Add(self.dim, 0, wx.LEFT, 0)
+        self._sizer.Add(self.infoRai, 0, wx.LEFT, 0)
+        self._sizer.Add(self.gm_mode, 0, wx.LEFT, 0)
+        self._sizer.Add(self.apply, 0, wx.LEFT, 160)
         self._sizer.Fit(self)
 
         self.Layout()
@@ -113,26 +120,35 @@ class SetPlayer(wx.Panel, DefaultOperationUI):
         self.X.SetValue(str(X))
         self.Y.SetValue(str(Y))
         self.Z.SetValue(str(Z))
-        self.dim.SetValue(str(pdata.get("DimensionId")))
+        self.dim.SetSelection(pdata.get("DimensionId"))
 
     def savePosData(self, _):
         player = self.playerlist.GetString(self.playerlist.GetSelection())
         pdata = self.getPlayerData(player)
         mode = self.gm_mode.GetString(self.gm_mode.GetSelection())
-
+        dim = {
+            0:"Over World",
+            1:"Nether",
+            2: "The End"
+        }
 
         pdata["PlayerGameMode"] = TAG_Int(int(self.listRadio.get(mode)))
-        pdata["DimensionId"] = TAG_Int(int(self.dim.GetValue()))
+        pdata["DimensionId"] = TAG_Int(int(self.dim.GetSelection()))
         pdata['Pos'] = TAG_List()
         pdata['Pos'].append(TAG_Float(float(self.X.GetValue().replace("f",""))))
         pdata['Pos'].append(TAG_Float(float(self.Y.GetValue().replace("f",""))))
         pdata['Pos'].append(TAG_Float(float(self.Z.GetValue().replace("f",""))))
         save = pdata.save_to(compressed=False, little_endian=True)
         self.world.level_wrapper._level_manager._db.put(player.encode("utf-8"), save)
+        wx.MessageBox(player +"\nPersonal Mode: "+ mode +"\nLocation is set to\n"+dim.get(int(self.dim.GetSelection()))+" \nX: "
+                      +self.X.GetValue().replace("f","")+"\nY: "+ self.Y.GetValue().replace("f","") +"\nZ: "+self.Z.GetValue().replace("f",""),
+                      "INFO", wx.OK | wx.ICON_INFORMATION)
 
     def saveData(self, data):
         with open(self.world.level_path + "\\" + "level.dat", "wb") as f:
             f.write(data)
+            wx.MessageBox("Achievements are Re-enable",
+                          "INFO", wx.OK | wx.ICON_INFORMATION)
 
     def loadData(self, _):
         with open(self.world.level_path + "\\" + "level.dat", "rb") as f:
@@ -145,4 +161,4 @@ class SetPlayer(wx.Panel, DefaultOperationUI):
     pass
 
 
-export = dict(name="SetPlayer Position/Re-enable achievements V1.0", operation=SetPlayer)  # by PreimereHell
+export = dict(name="SetPlayer Position/Re-enable achievements V1.1", operation=SetPlayer)  # by PreimereHell
