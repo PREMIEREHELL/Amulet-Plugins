2a7ead18d508ea1f30c01b592c38b71008d1498a
PREMIEREHELL <88470674+PREMIEREHELL@users.noreply.github.com>
2022-07-15 10:09:57 -0700

    (beta) Edit large worlds faster.(bug fix)
    Make/Use a small or new world to gain fast access to this plugin.,  Select the World you wish to Make a buffer world for.  Open the world it creates BufferLevel this will be blank ,So just Click Pull In Chunks..

--- buffer_world.py
+++ buffer_world.py
@@ -1,5 +1,6 @@
 import collections
 from amulet.utils import block_coords_to_chunk_coords
+from os.path import exists
 import amulet
 import wx
 import os
@@ -52,9 +53,9 @@ class Inventory(wx.Panel, DefaultOperationUI):
         self.send_enty.Bind(wx.EVT_BUTTON, self.put_entities_back)
 
         self.source_players = wx.Button(self, label="Get All Player Data")
-        self.source_players.Bind(wx.EVT_BUTTON, self.get_player_data())
+        self.source_players.Bind(wx.EVT_BUTTON, self.get_player_data)
         self.set_players = wx.Button(self, label="Send Back All Player Data")
-        self.set_players.Bind(wx.EVT_BUTTON, self.set_player_data())
+        self.set_players.Bind(wx.EVT_BUTTON, self.set_player_data)
 
         self.save_new_data = wx.Button(self, label="Send Chunks Back")
         self.save_new_data.Bind(wx.EVT_BUTTON, self.put_data_back)
@@ -139,36 +140,38 @@ class Inventory(wx.Panel, DefaultOperationUI):
         self.world_load_data()
 
     def del_selected_chunks(self, _):
-        self.world.save()
-        with open(self.world.level_wrapper.path+'/main_dir.txt', "r") as d:
-            self.world_location = d.read()
-            d.close()
-        self.raw_level = LevelDB(self.world_location, create_if_missing=False)
-        the_chunks = self.canvas.selection.selection_group.chunk_locations()
-        to_delete_list = []
+        if exists(self.world.level_wrapper.path + '/main_dir.txt'):
+            self.world.save()
+            with open(self.world.level_wrapper.path + '/main_dir.txt', "r") as d:
+                self.world_location = d.read()
+                d.close()
+            self.raw_level = LevelDB(self.world_location, create_if_missing=False)
+            the_chunks = self.canvas.selection.selection_group.chunk_locations()
+            to_delete_list = []
 
-        for xx, zz in the_chunks:
-            chunkkey = struct.pack('<ii', xx, zz) + self.get_dim_value_bytes()
-            key_len = len(chunkkey)
+            for xx, zz in the_chunks:
+                chunkkey = struct.pack('<ii', xx, zz) + self.get_dim_value_bytes()
+                key_len = len(chunkkey)
 
-            for k, v in self.raw_level.iterate(start=chunkkey, end=chunkkey + b'\xff\xff'):
-                if len(k) >= key_len:
-                    to_delete_list.append(k)
+                for k, v in self.raw_level.iterate(start=chunkkey, end=chunkkey + b'\xff\xff'):
+                    if len(k) >= key_len:
+                        to_delete_list.append(k)
 
-        for d in to_delete_list:
-            self.raw_level.delete(d)
-        self.raw_level.close(compact=False)
-        self.canvas.run_operation(
-            lambda: delete_chunk(
-                self.canvas.world,
-                self.canvas.dimension,
-                self.canvas.selection.selection_group,
-                False,
+            for d in to_delete_list:
+                self.raw_level.delete(d)
+            self.raw_level.close(compact=False)
+            self.canvas.run_operation(
+                lambda: delete_chunk(
+                    self.canvas.world,
+                    self.canvas.dimension,
+                    self.canvas.selection.selection_group,
+                    False,
 
-            ))
+                ))
 
-        self.world.unload()
-        self.canvas.renderer.render_world._rebuild()
+            self.world.unload()
+            self.canvas.renderer.render_world._rebuild()
+
 
     def operation_run_source(self, _):
 
@@ -178,33 +181,33 @@ class Inventory(wx.Panel, DefaultOperationUI):
         self.canvas.renderer.render_world._rebuild()
 
 
-    def get_player_data(self):
-        with open(self.world.level_wrapper.path+'/main_dir.txt', "r") as d:
-            self.world_location = d.read()
-            d.close()
-        self.raw_level = LevelDB(self.world_location, create_if_missing=False)
-        player_d = self.raw_level.get(b'~local_player')
-        for k, v in self.raw_level.iterate(start=b'player_server_',
-                                          end=b'player_server_\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'):
-            self.level_db.put(k,v)
+    def get_player_data(self, _):
 
-        self.level_db.put(b'~local_player', player_d)
-        self.raw_level.close(compact=False)
+            with open(self.world.level_wrapper.path + '/main_dir.txt', "r") as d:
+                self.world_location = d.read()
+                d.close()
+            self.raw_level = LevelDB(self.world_location, create_if_missing=False)
+            player_d = self.raw_level.get(b'~local_player')
+            for k, v in self.raw_level.iterate(start=b'player_server_',
+                                               end=b'player_server_\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'):
+                self.level_db.put(k, v)
 
-    def set_player_data(self):
-        with open(self.world.level_wrapper.path+'/main_dir.txt', "r") as d:
-            self.world_location = d.read()
-            d.close()
-        self.raw_level = LevelDB(self.world_location, create_if_missing=False)
-        player_d = self.level_db.get(b'~local_player')
-        for k, v in self.level_db.iterate(start=b'player_server_',
-                                           end=b'player_server_\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'):
-            self.raw_level.put(k, v)
+            self.level_db.put(b'~local_player', player_d)
+            self.raw_level.close(compact=False)
 
-        self.raw_level.put(b'~local_player', player_d)
-        self.raw_level.close(compact=False)
+    def set_player_data(self, _):
+        if exists(self.world.level_wrapper.path + '/main_dir.txt'):
+            with open(self.world.level_wrapper.path + '/main_dir.txt', "r") as d:
+                self.world_location = d.read()
+                d.close()
+            self.raw_level = LevelDB(self.world_location, create_if_missing=False)
+            player_d = self.level_db.get(b'~local_player')
+            for k, v in self.level_db.iterate(start=b'player_server_',
+                                              end=b'player_server_\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'):
+                self.raw_level.put(k, v)
 
-
+            self.raw_level.put(b'~local_player', player_d)
+            self.raw_level.close(compact=False)
 
     def world_load_data(self):
 
@@ -234,91 +237,93 @@ class Inventory(wx.Panel, DefaultOperationUI):
         return dim
 
     def put_data_back(self, _):
-        self.world.save()
-        with open(self.world.level_wrapper.path+'/main_dir.txt', "r") as d:
-            self.world_location = d.read()
-            d.close()
-        self.raw_level = LevelDB(self.world_location, create_if_missing=False)
-        the_chunks = [c for c in self.world.level_wrapper.all_chunk_coords(self.canvas.dimension)]
-        for xx, zz in the_chunks:
-            chunkkey = struct.pack('<ii', xx, zz) + self.get_dim_value_bytes()
-            key_len = len(chunkkey)
-            for k, v in self.level_db.iterate(start=chunkkey, end=chunkkey + b'\xff\xff'):
-                if len(k) >= key_len:
-                    self.raw_level.put(k, v)
-        self.raw_level.close(compact=False)
-        print("level closed")
+        if exists(self.world.level_wrapper.path + '/main_dir.txt'):
+            self.world.save()
+            with open(self.world.level_wrapper.path + '/main_dir.txt', "r") as d:
+                self.world_location = d.read()
+                d.close()
+            self.raw_level = LevelDB(self.world_location, create_if_missing=False)
+            the_chunks = [c for c in self.world.level_wrapper.all_chunk_coords(self.canvas.dimension)]
+            for xx, zz in the_chunks:
+                chunkkey = struct.pack('<ii', xx, zz) + self.get_dim_value_bytes()
+                key_len = len(chunkkey)
+                for k, v in self.level_db.iterate(start=chunkkey, end=chunkkey + b'\xff\xff'):
+                    if len(k) >= key_len:
+                        self.raw_level.put(k, v)
+            self.raw_level.close(compact=False)
+            print("level closed")
 
     def source_new(self):
-        with open(self.world.level_wrapper.path+'/main_dir.txt', "r") as d:
-            self.world_location = d.read()
-            d.close()
-        self.raw_level = LevelDB(self.world_location, create_if_missing=False)
-        loc = self.canvas.camera.location
-        chunk_range = []
-        the_chunks = []
-        the_data = []
-        cx, cz = block_coords_to_chunk_coords(loc[0], loc[2])
-        prang = self.the_range.GetValue()
-        for x in range(-prang, prang):
-            for z in range(-prang, prang):
-                chunk_range.append((cx + x, cz + z))
-
-        for xx, zz in chunk_range:
-
-            chunkkey = struct.pack('<ii', xx, zz) + self.get_dim_value_bytes()
-            key_len = len(chunkkey)
-            for k, v in self.raw_level.iterate(start=chunkkey, end=chunkkey + b'\xff\xff'):
-                if len(k) >= key_len:
-
-                    if self.keep_chunks.GetValue():
-                        if not self.world.has_chunk(xx, zz, self.canvas.dimension):
+        if exists(self.world.level_wrapper.path + '/main_dir.txt'):
+            with open(self.world.level_wrapper.path + '/main_dir.txt', "r") as d:
+                self.world_location = d.read()
+                d.close()
+            self.raw_level = LevelDB(self.world_location, create_if_missing=False)
+            chunk_range, the_chunks, the_data = [], [], []
+            loc = self.canvas.camera.location
+            cx, cz = block_coords_to_chunk_coords(loc[0], loc[2])
+            prang = self.the_range.GetValue()
+            for x in range(-prang, prang):
+                for z in range(-prang, prang):
+                    chunk_range.append((cx + x, cz + z))
+            for xx, zz in chunk_range:
+                chunkkey = struct.pack('<ii', xx, zz) + self.get_dim_value_bytes()
+                key_len = len(chunkkey)
+                for k, v in self.raw_level.iterate(start=chunkkey, end=chunkkey + b'\xff\xff'):
+                    if len(k) >= key_len:
+                        print(k)
+                        if self.keep_chunks.GetValue():
+                            if not self.world.has_chunk(xx, zz, self.canvas.dimension):
+                                the_chunks.append((xx, zz))
+                                the_data.append((k, v))
+                        else:
                             the_chunks.append((xx, zz))
                             the_data.append((k, v))
-                    else:
-                        the_chunks.append((xx, zz))
-                        the_data.append((k, v))
 
+            for xx, zz in the_chunks:
+                self.world.create_chunk(xx, zz, self.canvas.dimension).changed = True
+            self.world.save()
+            self.world.purge()
 
-        for xx, zz in the_chunks:
-            self.world.create_chunk(xx, zz, self.canvas.dimension).changed = True
-        self.world.save()
-        self.world.purge()
+            for k, v in the_data:
+                self.level_db.put(k, v)
+            self.raw_level.close(compact=False)
+            print("level closed")
 
-        for k, v in the_data:
-            self.level_db.put(k, v)
-        self.raw_level.close(compact=False)
-        print("level closed")
     def source_entities(self, _):
-        with open(self.world.level_wrapper.path+'/main_dir.txt', "r") as d:
-            self.world_location = d.read()
-            d.close()
-        self.raw_level = LevelDB(self.world_location, create_if_missing=False)
-        actorprefixs = iter(self.raw_level.iterate(start=b'actorprefix',
-                                                  end=b'actorprefix\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'))
-        digps = iter(self.raw_level.iterate(start=b'digp',
-                                           end=b'digp\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'))
-        for k,v in actorprefixs:
-            self.level_db.put(k,v)
-        for k, v in digps:
-            self.level_db.put(k, v)
-        self.raw_level.close(compact=False)
+        if exists(self.world.level_wrapper.path + '/main_dir.txt'):
+            with open(self.world.level_wrapper.path + '/main_dir.txt', "r") as d:
+                self.world_location = d.read()
+                d.close()
+            self.raw_level = LevelDB(self.world_location, create_if_missing=False)
+            actorprefixs = iter(self.raw_level.iterate(start=b'actorprefix',
+                                                       end=b'actorprefix\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'))
+            digps = iter(self.raw_level.iterate(start=b'digp',
+                                                end=b'digp\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'))
+            for k, v in actorprefixs:
+                self.level_db.put(k, v)
+            for k, v in digps:
+                self.level_db.put(k, v)
+            self.raw_level.close(compact=False)
+
 
     def put_entities_back(self, _):
-        with open(self.world.level_wrapper.path+'/main_dir.txt', "r") as d:
-            self.world_location = d.read()
-            d.close()
-        self.raw_level = LevelDB(self.world_location, create_if_missing=False)
-        actorprefixs = iter(self.level_db.iterate(start=b'actorprefix',
-                                                  end=b'actorprefix\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'))
-        digps = iter(self.level_db.iterate(start=b'digp',
-                                           end=b'digp\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'))
-        for k,v in actorprefixs:
-            self.raw_level.put(k,v)
-        for k, v in digps:
-            self.raw_level.put(k, v)
+        if exists(self.world.level_wrapper.path + '/main_dir.txt'):
+            with open(self.world.level_wrapper.path + '/main_dir.txt', "r") as d:
+                self.world_location = d.read()
+                d.close()
+            self.raw_level = LevelDB(self.world_location, create_if_missing=False)
+            actorprefixs = iter(self.level_db.iterate(start=b'actorprefix',
+                                                      end=b'actorprefix\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'))
+            digps = iter(self.level_db.iterate(start=b'digp',
+                                               end=b'digp\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'))
+            for k, v in actorprefixs:
+                self.raw_level.put(k, v)
+            for k, v in digps:
+                self.raw_level.put(k, v)
 
-        self.raw_level.close(compact=False)
+            self.raw_level.close(compact=False)
+
 
     @property
     def level_db(self):
