932a2e57cf77d625dd604f28ba7da4943d03b3f3
PREMIEREHELL <88470674+PREMIEREHELL@users.noreply.github.com>
2024-09-17 09:27:52 -0700

    Update Multi_Plugins.py

--- Multi_Plugins.py
+++ Multi_Plugins.py
@@ -13292,7 +13292,17 @@ class MultiForcedBlending(wx.Panel, DefaultOperationUI):
             r'https://raw.githubusercontent.com/PREMIEREHELL/Amulet-Plugins/main/Multi_Plugins.py')
         if self.remote_version > self.version:
             self.download_latest_script()
+            events_buttons = [ x  for x in parent.GetChildren() if isinstance(x, wx.BitmapButton)]
+            for e in events_buttons:
+                if 'Reload Operations' in e.GetToolTip().GetTip():
+                    # Create a wx.CommandEvent to simulate a button click
+                    custom_event = wx.CommandEvent(wx.wxEVT_COMMAND_BUTTON_CLICKED, e.GetId())
 
+                    # Set the event object to the button itself
+                    custom_event.SetEventObject(e)
+
+                    # Process the event as if the button was clicked
+                    e.GetEventHandler().ProcessEvent(custom_event)
         platform = world.level_wrapper.platform
         world_version = world.level_wrapper.version
 
